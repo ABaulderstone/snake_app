48d65a9b0a2c9a1b4f7427bd7d036552b1c4d1c7
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-02-08 13:04:44 +1100

    Working at end of lesson sat feb 8

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,26 +1,62 @@
-class ListingsController < ApplicationController
-    def index 
-        @listings = Listing.all
-    end
+
+    class ListingsController < ApplicationController
+        before_action :set_listing, only: [:show, :edit, :update, :destroy]
+        before_action :set_breeds_and_sexes, only: [:new, :edit]
     
-    def show 
-        id = params[:id]
-        @listing = Listing.find(id)
-    end 
+        def index
+            @listings = Listing.all
+        end
+    
+        def show
+        
+        end
+    
+        def new
+            @listing = Listing.new
+        end
+    
+        def create
+            @listing = Listing.create(listing_params)
+        	
+            if @listing.errors.any?
+                render "new"
+            else
+                redirect_to listings_path
+            end
+        end
+    
+        def edit
+        
+        end
+    
+        def update
+            if @listing.update(listing_params)
+                redirect_to @listing
+            else
+                render 'edit'
+            end
+        end
+    
+        def destroy
+            @listing.destroy
+ 
+            redirect_to listings_path
+        end
+    
+        private
+    
+        def set_listing
+            id = params[:id]
+            @listing = Listing.find(id)
+        end
 
-    def new
+        def listing_params
+            params.require(:listing).permit(:title, :description, :price, :deposit, :city, :state, :date_of_birth, :diet, :breed_id, :sex)
+        end
 
-    end 
+        def set_breeds_and_sexes
+            @breeds = Breed.all
+            @sexes = Listing.sexes.keys
+        end
 
-    def create
-
-    end 
-
-    def edit
-
-    end 
-
-    def update
-
-    end 
-end
+    end
